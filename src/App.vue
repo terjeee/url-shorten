a3206e628dd17d53d838e75c9244beab21a6113f
terjeee <96795863+terjeee@users.noreply.github.com>
2023-03-18 16:23:02 +0100

    :)

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,47 @@
 <script lang="ts">
-  export default {};
+  import BgShortenDesktop from "/images/BgShortenDesktop.vue";
+  import BgShortenMobile from "../public/images/BgShortenMobile.vue";
+  import IconBrand from "../public/images/IconBrand.vue";
+  import IconCustomizable from "../public/images/IconCustomizable.vue";
+  import IconFacebook from "../public/images/IconFacebook.vue";
+  import IconInstagram from "../public/images/IconInstagram.vue";
+  import IconPinterest from "../public/images/IconPinterest.vue";
+  import IconRecords from "../public/images/IconRecords.vue";
+  import IconTwitter from "../public/images/IconTwitter.vue";
+  import IllustrationWork from "../public/images/IllustrationWork.vue";
+  import Logo from "../public/images/Logo.vue";
+
+  export default {
+    name: "App",
+    components: {
+      BgShortenDesktop,
+      BgShortenMobile,
+      IconBrand,
+      IconCustomizable,
+      IconFacebook,
+      IconInstagram,
+      IconPinterest,
+      IconRecords,
+      IconTwitter,
+      IllustrationWork,
+      Logo,
+    },
+  };
 </script>
 
 <template>
   <h1>hei</h1>
+  <BgShortenDesktop />
+  <BgShortenMobile />
+  <IconBrand />
+  <IconCustomizable />
+  <IconFacebook />
+  <IconInstagram />
+  <IconPinterest />
+  <IconRecords />
+  <IconTwitter />
+  <IllustrationWork />
+  <Logo />
 </template>
+
+<style lang="scss"></style>
