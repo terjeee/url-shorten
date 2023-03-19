7c9d07f61bae084e1f723ea2c847f857763baf7b
terjeee <96795863+terjeee@users.noreply.github.com>
2023-03-19 05:52:27 +0100

    :)

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,30 @@
-<script lang="ts">
-  import Logo from "./components/svg/Logo.vue";
-  import IllustrationWork from "./components/svg/IllustrationWork.vue";
-  import BgShortenDesktop from "./components/svg/BgShortenDesktop.vue";
-  import BgShortenMobile from "./components/svg/BgShortenMobile.vue";
-  import IconBrand from "./components/svg/IconBrand.vue";
-  import IconCustomizable from "./components/svg/IconCustomizable.vue";
-  import IconRecords from "./components/svg/IconRecords.vue";
-  import IconFacebook from "./components/svg/conFacebook.vue";
-  import IconInstagram from "./components/svg/IconInstagram.vue";
-  import IconPinterest from "./components/svg/IconPinterest.vue";
-  import IconTwitter from "./components/svg/IconTwitter.vue";
-
-  export default {
-    name: "App",
-    components: {
-      BgShortenDesktop,
-      BgShortenMobile,
-      IconBrand,
-      IconCustomizable,
-      IconFacebook,
-      IconInstagram,
-      IconPinterest,
-      IconRecords,
-      IconTwitter,
-      IllustrationWork,
-      Logo,
-    },
-  };
+<script setup lang="ts">
+  import Layout from "@/Layout/Layout.vue";
+  import IllustrationWork from "@/components/svg/IllustrationWork.vue";
+  import BgShortenDesktop from "@/components/svg/BgShortenDesktop.vue";
+  import BgShortenMobile from "@/components/svg/BgShortenMobile.vue";
+  import IconBrand from "@/components/svg/IconBrand.vue";
+  import IconRecords from "@/components/svg/IconRecords.vue";
+  import IconCustomizable from "@/components/svg/IconCustomizable.vue";
+  import IconFacebook from "@/components/svg/conFacebook.vue";
+  import IconInstagram from "@/components/svg/IconInstagram.vue";
+  import IconPinterest from "@/components/svg/IconPinterest.vue";
+  import IconTwitter from "@/components/svg/IconTwitter.vue";
 </script>
 
 <template>
-  <h1>hei</h1>
-  <BgShortenDesktop />
-  <BgShortenMobile />
-  <IconBrand />
-  <IconCustomizable />
-  <IconFacebook />
-  <IconInstagram />
-  <IconPinterest />
-  <IconRecords />
-  <IconTwitter />
-  <IllustrationWork />
-  <Logo />
+  <Layout>
+    <IllustrationWork />
+    <BgShortenMobile />
+    <BgShortenDesktop />
+    <IconRecords />
+    <IconCustomizable />
+    <IconFacebook />
+    <IconBrand />
+    <IconInstagram />
+    <IconPinterest />
+    <IconTwitter />
+  </Layout>
 </template>
 
-<style lang="scss"></style>
+<style scoped lang="scss"></style>
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { createApp } from "vue";
 
-import App from "./App.vue";
+import App from "@/App.vue";
+import "@/assets/main.scss";
 
 const app = createApp(App);
 
