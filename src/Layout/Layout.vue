ea4cd30e48a4ded388e885928915a1b56e8d3922
terjeee <96795863+terjeee@users.noreply.github.com>
2023-03-19 06:20:47 +0100

    god natt

--- src/Layout/Layout.vue
+++ src/Layout/Layout.vue
@@ -1,11 +1,34 @@
-<template>
-  <nav>
-    <Logo />
-  </nav>
-  <slot />
-  <footer></footer>
-</template>
-
 <script setup lang="ts">
   import Logo from "@/components/svg/Logo.vue";
+  import IconBrand from "@/components/svg/IconBrand.vue";
+  import IconInstagram from "@/components/svg/IconInstagram.vue";
+  import IconTwitter from "@/components/svg/IconTwitter.vue";
 </script>
+
+<template>
+  <nav class="nav">
+    <Logo />
+  </nav>
+  <slot></slot>
+  <footer class="footer">
+    <Logo fill="white" />
+    <div>
+      <IconBrand />
+      <IconInstagram />
+      <IconTwitter />
+    </div>
+  </footer>
+</template>
+
+<style scoped lang="scss">
+  @use "@/assets/variables.scss";
+
+  .nav {
+    padding: 1rem 0;
+    width: 100%;
+  }
+
+  .footer {
+    background-color: variables.$clr-dark-violet;
+  }
+</style>
