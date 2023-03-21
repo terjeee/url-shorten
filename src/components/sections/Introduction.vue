5d985be1d852aaea0bb3cebd2b36d0a2dedcbfae
terjeee <96795863+terjeee@users.noreply.github.com>
2023-03-21 06:12:46 +0100

    :)

---
+++ src/components/sections/Introduction.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+  import MaxWidth from "@/components/ux/MaxWidth.vue";
+  import IllustrationWork from "@/components/ux/svg/IllustrationWork.vue";
+</script>
+
+<template>
+  <header>
+    <IllustrationWork />
+    <MaxWidth>
+      <div class="text">
+        <h2>More than just shorter links</h2>
+        <p>Build your brand's recognition and get detailed insights on how your links are performing.</p>
+      </div>
+    </MaxWidth>
+  </header>
+</template>
+
+<style scoped lang="scss">
+  @use "@/assets/variables.scss";
+
+  header {
+    .text {
+      padding: 4rem 0;
+      text-align: center;
+
+      h2 {
+        font-size: 4rem;
+        line-height: 1.2;
+      }
+
+      p {
+        padding-top: 1.5rem;
+        font-size: variables.$font-size;
+      }
+    }
+  }
+</style>
