cb13d75bc234de271e8202c34e84e31bd1f19fb7
terjeee <96795863+terjeee@users.noreply.github.com>
2023-03-31 04:49:41 +0200

    responsive

--- src/components/sections/Introduction.vue
+++ src/components/sections/Introduction.vue
@@ -26,13 +26,29 @@
     flex-direction: column;
     align-items: center;
 
+    @include breakpoint.width(laptop) {
+      padding: 4rem 0;
+      flex-direction: row-reverse;
+      margin-bottom: 5rem;
+    }
+
     .text {
       padding: 4rem 0;
       text-align: center;
 
+      @include breakpoint.width(laptop) {
+        width: 80%;
+        margin-right: auto;
+        text-align: left;
+      }
+
       h2 {
         font-size: 4rem;
         line-height: 1.2;
+
+        @include breakpoint.width(laptop) {
+          font-size: 4.75rem;
+        }
       }
 
       p {
@@ -40,23 +56,6 @@
         padding-right: 4rem;
         font-size: variables.$font-size;
         line-height: 1.5;
-      }
-    }
-  }
-
-  @include breakpoint.width(laptop) {
-    header {
-      flex-direction: row-reverse;
-      margin-bottom: 5rem;
-
-      .text {
-        width: 80%;
-        margin-right: auto;
-        text-align: left;
-
-        h2 {
-          font-size: 4.75rem;
-        }
       }
     }
   }
